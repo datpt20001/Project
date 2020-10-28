bbb4d9ba09a8a884b80579753f5679e5419480b1
datpt20001 <73586343+datpt20001@users.noreply.github.com>
2020-10-28 17:10:27 +0700

    Add files via upload

---
+++ Validation.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class Validation {
+
+    private Scanner sc = new Scanner(System.in);
+
+    public int inputInt(String mess, int min, int max) {
+        System.out.print(mess);
+        while (true) {
+            String input = sc.nextLine();
+            try {
+                int number = Integer.parseInt(input);
+                if (number < min || number > max) {
+                    System.out.print("Please enter a number between " + min + ", " + max + ": ");
+                    continue;
+                }
+                return number;
+            } catch (Exception e) {
+                System.out.print("Please input an integer number: ");
+            }
+        }
+    }
+}
